069247ddcccd91e481012f5810383ddbb712c40a
Jim Charnesky <mijnesky@hotmail.com>
2020-12-06 11:20:38 -0700

    Complete working implementation code

--- src/book.js
+++ src/book.js
@@ -1,12 +1,46 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return `The ${title}`;
 }
+
+function buildMainCharacter(name, age, pronouns) {
+  var mainCharacter = {
+   name: name,
+   age: age,
+   pronouns: pronouns
+};
+  return mainCharacter;
+}
+
+function saveReview(addReview, reviews) {
+  if(!reviews.includes(addReview)) {
+    reviews.push(addReview);
+ }
+}
+
+function calculatePageCount(bookTitle) {
+  return bookTitle.length * 20;
+}
+
+function writeBook(createTitle, buildMainCharacter, genre) {
+  var book = {
+    title: createTitle,
+    mainCharacter: buildMainCharacter,
+    pageCount: calculatePageCount(createTitle),
+    genre: `fantasy`
+  }
+  return book;
+}
+
+function editBook(book) {
+  return book.pageCount = (book.pageCount * .75);
+}
+
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
