66e920a5f07e76dd037b3a0d94c36eed1338ee5c
Jim Charnesky <mijnesky@hotmail.com>
2020-12-06 20:30:26 -0700

    Add working listTitles function to implementation code

--- src/shelf.js
+++ src/shelf.js
@@ -5,12 +5,21 @@ function shelfBook(book, sciFiShelf) {
 }
 
 function unshelfBook(book, sciFiShelf) {
-    sciFiShelf.splice(1,1);
+  sciFiShelf.splice(1,1);
 }
+
+function listTitles(shelf) {
+  var titles = [];
+  for (i = 0; i < shelf.length; i++) {
+    titles.push(shelf[i].title);
+  // console.log(titles);
+  }
+  return titles.join(", ");
+};
 
 module.exports = {
    shelfBook,
    unshelfBook,
-  // listTitles,
+   listTitles,
   // searchShelf
 };
